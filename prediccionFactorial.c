cbd071cd4b390696a9c5925426aaf568b1fa77a2
José Rodrigo Pérez Beltrán <ptrroot@gmail.com>
2019-11-12 12:12:29 -0600

    Add files via upload

---
+++ prediccionFactorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//autor PtrRodrigo Null
+int main()
+{
+int numero;
+printf("\nIngresa el numero: ");
+scanf("%d",&numero);
+
+if(numero > 0){
+
+int Ceros = 0;
+int potencia = 5;
+
+while(numero - potencia >= 0)
+{
+Ceros += numero/potencia;
+potencia *= 5;
+}
+
+printf("\n EL numero de ceros del factorial sera: %d",
+Ceros);
+}
+else printf("\nEse numero no es entero positivo");
+
+return 0;
+}
